22c892859c6650c483c271bf009c73f4e007ea89
Keunna Lee <52783516+dev-Lena@users.noreply.github.com>
2024-10-26 00:46:53 +0900

    chore: addTodo 메서드 이름 수정

--- Todo/Todo/Views/ListTabView.swift
+++ Todo/Todo/Views/ListTabView.swift
@@ -3,19 +3,20 @@ import SwiftUI
 
 struct ListTabView: View {
     @StateObject var viewModel: ListTabViewModel
-    @State private var newTodoTitle = ""
     
     var body: some View {
         NavigationView {
-            List(viewModel.todos) { todo in
-                Text(todo.title) // 할 일 제목 표시
+            List {
+                ForEach(viewModel.todos) { todo in 
+                    Text(todo.title)
+                }
             }
             .navigationTitle("Todos")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Add") {
                         Task {
-                            await viewModel.addTodoAsync("New Todo") // 새 할 일 추가
+                            await viewModel.addTodo("New Todo")
                         }
                     }
                 }
@@ -23,4 +24,3 @@ struct ListTabView: View {
         }
     }
 }
-
